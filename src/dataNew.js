ccc77ced8a303e0f9e187ba787fa2e733210eb74
Anurag41682 <anuragyadav41682@gmail.com>
2024-08-01 18:17:34 +0530

    changed starting point

---
+++ src/dataNew.js
@@ -0,0 +1,498 @@
+const data = [
+  {
+    course: "Data Science",
+    period: "180 Days",
+    subTag: "Data & AI",
+    costPrice: "75000",
+    sellingPrice: "33750",
+    courseDesc:
+      "In this comprehensive data science course, you'll learn how to extract meaningful insights from complex data sets. From data wrangling to building predictive models, this course covers the entire data science pipeline.",
+    curriculumLink: "https://zfrmz.in/ZfhpSbqo4iImLIDP9C53",
+    curriculumSnapshot: "",
+    learnmoreCourse: "dataScience",
+    resource: "Complete Study Material",
+    imageKey: "dataScience",
+    technology: "Data Science",
+    modules: [
+      {
+        moduleHeading: "Strong Fundamentals",
+        description: [
+          "Build a solid foundation in essential mathematical concepts for advanced data science techniques.",
+          "Dive deep into statistical methods for precise data analysis.",
+          "Master advanced Python skills for sophisticated data science algorithms and models.",
+        ],
+      },
+      {
+        moduleHeading: "Handle Data Like a Pro",
+        description: [
+          "Master data wrangling and processing techniques for efficient data manipulation.",
+          "Dive into advanced exploratory data analysis (EDA) to uncover insights and patterns in complex datasets.",
+        ],
+      },
+      {
+        moduleHeading: "Surround Yourself with Models",
+        description: [
+          "Master supervised learning algorithms for predictive modeling.",
+          "Explore unsupervised learning algorithms for pattern detection.",
+          "Delve into advanced machine learning techniques, including deep learning.",
+        ],
+      },
+      {
+        moduleHeading: "Convince with Data",
+        description: [
+          "Learn advanced model evaluation techniques for accurate performance assessment.",
+          "Master hyperparameter tuning to optimize machine learning models.",
+        ],
+      },
+      {
+        moduleHeading: "Will AI Replace You?",
+        description: [
+          "Dive deep into Large Language Models (LLMs), the cutting-edge technology reshaping the world.",
+          "Discover the power of LLMs' generative capabilities and unleash your creativity to solve real-world problems with innovation and ethical awareness.",
+        ],
+      },
+      {
+        moduleHeading: "Clumsy Data and Dirty Hands",
+        description: [
+          "Work on real-world projects and case studies to apply your skills.",
+          "Complete a capstone project to demonstrate your expertise and knowledge.",
+        ],
+      },
+    ],
+    level: "Intermediate",
+    mode: "Live Sessions",
+    efforts: "10hrs/ Week",
+    projectsGuided: "5+",
+
+    tools: [
+      "Python",
+      "Pandas",
+      "NumPy",
+      "Scikit-learn",
+      "TensorFlow",
+      "Looker Studio",
+    ],
+    skills: [
+      "Data Cleaning and Preprocessing",
+      "Exploratory Data Analysis (EDA)",
+      "Machine Learning",
+      "Big Data Technologies & Data Engineering",
+      "Data Visualization",
+    ],
+    faqs: [
+      {
+        question: "What is the duration of the Data Science course?",
+        answer: "The course lasts for 6 months.",
+      },
+      {
+        question: "What skills will I learn in this course?",
+        answer:
+          "You will learn data Science principles, how to use data Science tools, dashboard design, storytelling with data, and advanced techniques.",
+      },
+      {
+        question: "How many hours per week should I dedicate to this course?",
+        answer:
+          "You should plan to spend around 10 hours per week on the course.",
+      },
+      {
+        question: "What tools will I use during the course?",
+        answer: "You will use Python, NumPy, Scikit-learn, and Tensor Flow.",
+      },
+      {
+        question: "Is this course suitable for beginners?",
+        answer:
+          "Yes, the course is suitable for beginners and progresses to intermediate-level techniques.",
+      },
+      {
+        question: "Will there be any real-world projects?",
+        answer:
+          "Yes, you will work on hands-on projects and case studies, including 5+ guided projects and a capstone project.",
+      },
+      {
+        question: "What kind of support will I receive during the course?",
+        answer:
+          "You will receive support from instructors and have access to a community of peers for collaboration and discussion.",
+      },
+      {
+        question: "What if I have more questions?",
+        answer:
+          "If you have additional questions, please contact our support team at reachus@coursevita.com",
+      },
+    ],
+    jobRoles: [
+      {
+        name: "Data Analyst",
+        avgSalary: "₹4 - ₹9 LPA",
+        openings: "20K+",
+      },
+      {
+        name: "Data Scientist",
+        avgSalary: "₹8 - ₹19 LPA",
+        openings: "7K+",
+      },
+      {
+        name: "Data Engineer",
+        avgSalary: "₹5 - ₹16 LPA",
+        openings: "35K+",
+      },
+      {
+        name: "Data Architect",
+        avgSalary: "₹18 - ₹30 LPA",
+        openings: "10K+",
+      },
+      {
+        name: "Data Mining Engineer",
+        avgSalary: "₹3.5 - ₹5 LPA",
+        openings: "70K+",
+      },
+    ],
+    ambitionBoxlinks: "https://www.ambitionbox.com/profile/data-scientist-jobs",
+    linkedinJobRoles:
+      "https://www.linkedin.com/jobs/search/?currentJobId=3932913303&geoId=102713980&keywords=data%20science%20job%20roles&location=India&origin=JOB_SEARCH_PAGE_SEARCH_BUTTON&refresh=true",
+  },
+  {
+    course: "Data Analytics",
+    period: "180 Days",
+    subTag: "Data & AI",
+    costPrice: "65000",
+    sellingPrice: "45000",
+    courseDesc:
+      "This course is designed to equip you with the skills required for data analysis, a crucial aspect of decision-making processes in businesses. From data collection to interpretation, you will learn various techniques and tools to analyse data effectively.",
+    curriculumLink: "https://zfrmz.in/ZfhpSbqo4iImLIDP9C53",
+    curriculumSnapshot: "",
+    learnmoreCourse: "dataAnalytics",
+    resource: "Complete Study Material",
+    imageKey: "Rectangle",
+    technology: "Data Science",
+    modules: [
+      {
+        moduleHeading: "Fundamentals of Data Analysis",
+        description: [
+          "Data Analysis Basics : Introduction, importance, and process overview.",
+          "Data Visualization : Types of data, descriptive statistics, distributions.",
+          "Master the art of exploratory data analysis (EDA) to uncover patterns and trends.",
+        ],
+      },
+      {
+        moduleHeading: "Excel for Data Analysis",
+        description: [
+          "Excel Basics : Interface, data entry, basic formulas and charts.",
+          "Advanced Excel : Statistical analysis, complex functions, and data visualization.",
+        ],
+      },
+      {
+        moduleHeading: "SQL Programming",
+        description: [
+          "SQL Fundamentals : Basic commands, data filtering, and sorting.",
+          "Data Visualization : Aggregate functions, joins, subqueries, views, and indexing.",
+        ],
+      },
+      {
+        moduleHeading: "Python Programming",
+        description: [
+          "Python Basics : Setup, syntax, variables, and data types.",
+          "Data Structures : Lists, tuples, dictionaries, and their operations.",
+          "Control Structures : Conditional statements and loops.",
+        ],
+      },
+      {
+        moduleHeading: "Python for Data Analysis",
+        description: [
+          "NumPy and Pandas : Array and DataFrame operations, data manipulation and cleaning.",
+          "Data Visualization : Matplotlib, Seaborn, and Plotly for creating basic and interactive plots.",
+        ],
+      },
+      {
+        moduleHeading: "Advanced Data Management",
+        description: [
+          "Database Management : Advanced SQL functions, security practices.",
+          "Tableau Visualization : Basic to advanced data visualization techniques, dashboards, and storytelling.",
+        ],
+      },
+      {
+        moduleHeading: "Exploratory Data Analysis (EDA)",
+        description: [
+          "Data Preprocessing : Techniques for handling outliers and missing data.",
+          "Web Scraping : Using BeautifulSoup and Scrapy for data collection.",
+        ],
+      },
+      {
+        moduleHeading: "Machine Learning and Analytics",
+        description: [
+          "Basics of Machine Learning : Overview, regression, classification.",
+          "Advanced Analytics : Clustering, association rule learning, time series analysis..",
+        ],
+      },
+      {
+        moduleHeading: "Capstone Project and Career Development",
+        description: [
+          "Capstone Project : End-to-end data analysis project.",
+          "Career Preparation : Portfolio building, resume writing, interview preparation.",
+        ],
+      },
+      {
+        moduleHeading: "Data Ethics and Governance",
+        description: [
+          "Data Privacy and Security : Advanced SQL functions, security practices.",
+          "Ethical Data Use : Bias prevention, ethical considerations.",
+          "Data Governance: Frameworks and strategies for implementation.",
+        ],
+      },
+      {
+        moduleHeading: "Capstone Project and Career Development",
+        description: [
+          "Capstone Project : End-to-end data analysis project.",
+          "Career Preparation : Portfolio building, resume writing, interview preparation.",
+        ],
+      },
+    ],
+    level: "Beginner",
+    mode: "Live Sessions",
+    efforts: "10hrs/ Week",
+    projectsGuided: "5+",
+    tools: [
+      "Python",
+      "Pandas",
+      "NumPy",
+      "Matplotlib",
+      "Seaborn",
+      "Excel",
+      "Power Query",
+      "SQL",
+      "Tableau",
+      "Power BI",
+      "Looker Studio",
+    ],
+    skills: [
+      "Data Collection and Cleaning",
+      "Data Exploration and Visualization",
+      "Statistical Analysis",
+      "Data Interpretation and Reporting",
+    ],
+    faqs: [
+      {
+        question: "What is the duration of the Data Analytics course?",
+        answer: "The course lasts for 6 months.",
+      },
+      {
+        question: "What skills will I learn in this course?",
+        answer:
+          "You will learn data collection and cleaning, data exploration and visualisation, statistical analysis, and data interpretation and reporting.",
+      },
+      {
+        question: "How many hours per week should I dedicate to this course?",
+        answer:
+          "You should plan to spend around 10 hours per week on the course.",
+      },
+      {
+        question: "What tools will I use during the course?",
+        answer:
+          "You will use Python (Pandas, NumPy, Matplotlib, Seaborn), Excel, Power Query, SQL, and Tableau/Power BI/Looker Studio.",
+      },
+      {
+        question: "Is this course suitable for beginners?",
+        answer:
+          "Yes, the course builds a solid foundation in essential statistical concepts and progresses to advanced data analysis techniques.",
+      },
+      {
+        question: "Will there be any real-world projects?",
+        answer:
+          "Yes, you will work on real-world projects and case studies, including 5+ guided projects and a capstone project.",
+      },
+      {
+        question: "What kind of support will I receive during the course?",
+        answer:
+          "You will receive support from instructors and have access to a community of peers for collaboration and discussion.",
+      },
+      {
+        question: "What if I have more questions?",
+        answer:
+          "If you have additional questions, please contact our support team at reachus@coursevita.com.",
+      },
+    ],
+    jobRoles: [
+      {
+        name: "Data Analyst",
+        avgSalary: "₹4 - ₹9 LPA",
+        openings: "20K+",
+      },
+      {
+        name: "Data Scientist",
+        avgSalary: "₹8 - ₹19 LPA",
+        openings: "7K+",
+      },
+      {
+        name: "Business Intelligence Analyst",
+        avgSalary: "₹5 - ₹11 LPA",
+        openings: "30K+",
+      },
+      {
+        name: "Marketing Analytics Manager",
+        avgSalary: "₹11 - ₹30 LPA",
+        openings: "16K+",
+      },
+    ],
+    ambitionBoxlinks: "https://www.ambitionbox.com/profile/data-analyst-jobs",
+    linkedinJobRoles:
+      "https://www.linkedin.com/jobs/search/?currentJobId=3913498796&geoId=102713980&keywords=data%20Analytics%20job%20roles&location=India&origin=JOB_SEARCH_PAGE_SEARCH_BUTTON&refresh=true",
+  },
+  {
+    course: "Data Visualization",
+    period: "90 Days",
+    subTag: "Data Visualization",
+    costPrice: "25000",
+    sellingPrice: "11250",
+    courseDesc:
+      "Immerse yourself in the world of data visualization in this comprehensive course tailored for aspiring data visualization specialists. From creating insightful charts to designing interactive dashboards, master the art of visual storytelling with data.",
+    curriculumLink: "https://zfrmz.in/ZfhpSbqo4iImLIDP9C53",
+    curriculumSnapshot: "",
+    learnmoreCourse: "dataVisualization",
+    resource: "Complete Study Material",
+    imageKey: "dataVisualization",
+    technology: "Data Science",
+    modules: [
+      {
+        moduleHeading: "Fundamentals of Data Visualization",
+        description: [
+          "Gain a deep understanding of data visualization principles and best practices.",
+          "Learn about different types of charts, graphs, and visualization techniques.",
+        ],
+      },
+      {
+        moduleHeading: "Data Visualization Tools and Techniques",
+        description: [
+          "Explore popular data visualization tools such as Tableau, Power BI, and matplotlib.",
+          "Learn how to create engaging and interactive visualizations using these tools.",
+        ],
+      },
+      {
+        moduleHeading: "Designing Effective Dashboards",
+        description: [
+          "Master the design principles for creating effective dashboards that convey insights at a glance.",
+          "Learn about dashboard layout, color schemes, and user interface design.",
+        ],
+      },
+      {
+        moduleHeading: "Storytelling with Data",
+        description: [
+          "Understand the importance of storytelling in data visualization.",
+          "Learn how to craft compelling narratives and communicate insights effectively through visualizations.",
+        ],
+      },
+      {
+        moduleHeading: "Advanced Visualization Techniques",
+        description: [
+          "Dive into advanced visualization techniques such as geospatial visualization and network visualization.",
+          "Learn how to tackle complex data visualization challenges and create visually stunning presentations.",
+        ],
+      },
+      {
+        moduleHeading: "Real-World Applications",
+        description: [
+          "Work on hands-on projects and case studies to apply your data visualization skills in real-world scenarios.",
+          "Complete a capstone project to showcase your expertise in designing impactful visualizations.",
+        ],
+      },
+    ],
+    level: "Beginner",
+    mode: "Live Sessions",
+    efforts: "8hrs/Week",
+    projectsGuided: "3+",
+    tools: ["Tableau", "Power BI", "Matplotlib", "Seaborn"],
+    skills: [
+      "Data Visualization Principles",
+      "Data Visualization Tools",
+      "Dashboard Design",
+      "Storytelling with Data",
+      "Advanced Visualization Techniques",
+    ],
+    rolesEligibility: {
+      name: "Data Visualization Specialist",
+      openings: "",
+      avgSalary: "",
+    },
+    faqs: [
+      {
+        question: "What is the duration of the Data Visualization course?",
+        answer: "The course lasts for 3 months.",
+      },
+      {
+        question: "What skills will I learn in this course?",
+        answer:
+          "You will learn data visualisation principles, how to use data visualisation tools, dashboard design, storytelling with data, and advanced visualisation techniques.",
+      },
+      {
+        question: "How many hours per week should I dedicate to this course?",
+        answer:
+          "You should plan to spend around 8 hours per week on the course.",
+      },
+      {
+        question: "What tools will I use during the course?",
+        answer: "You will use Tableau, Power BI, matplotlib, and seaborn.",
+      },
+      {
+        question: "Is this course suitable for beginners?",
+        answer:
+          "Yes, the course is suitable for beginners and progresses to intermediate-level techniques.",
+      },
+      {
+        question: "Will there be any real-world projects?",
+        answer:
+          "Yes, you will work on hands-on projects and case studies, including 3+ guided projects and a capstone project.",
+      },
+      {
+        question: "What kind of support will I receive during the course?",
+        answer:
+          "You will receive support from instructors and have access to a community of peers for collaboration and discussion.",
+      },
+      {
+        question: "What if I have more questions?",
+        answer:
+          "If you have additional questions, please contact our support team at reachus@coursevita.com.",
+      },
+    ],
+    jobRoles: [
+      {
+        name: "Data Visualization Engineer",
+        avgSalary: "₹4 - ₹16 LPA",
+        openings: "35K+",
+      },
+      {
+        name: "Data Analyst",
+        avgSalary: "₹4 - ₹9 LPA",
+        openings: "20K+",
+      },
+      {
+        name: "Data Scientist",
+        avgSalary: "₹8 - ₹19 LPA",
+        openings: "7K+",
+      },
+      {
+        name: "Business Intelligence Analyst",
+        avgSalary: "₹5 - ₹11 LPA",
+        openings: "30K+",
+      },
+      {
+        name: "Analytics Manager",
+        avgSalary: "₹16 - ₹23 LPA",
+        openings: "16K+",
+      },
+    ],
+    ambitionBoxlinks: "https://www.naukri.com/data-visualization-jobs",
+    linkedinJobRoles:
+      "https://www.linkedin.com/jobs/search/?currentJobId=3936828791&keywords=data%20visualization%20job%20roles&origin=BLENDED_SEARCH_RESULT_NAVIGATION_SEE_ALL&originToLandingJobPostings=3847045021%2C3936179448%2C3937576726",
+  },
+];
+
+const newData = data.filter((item) => item.costPrice > 45000);
+
+// console.log(newData);
+
+let currentSum = 0;
+const fn = (accumulator, data) => {
+  return accumulator + Number(data.costPrice);
+};
+
+const totalSum = data.reduce(fn, currentSum);
+
+console.log(totalSum);
